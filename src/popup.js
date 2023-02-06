6f5d5e3ba58641d33a9dcb9a78d72e21055538a9
Aqsa-Irshad09 <--global>
2023-02-06 19:00:49 +0500

    update popup.js

--- src/popup.js
+++ src/popup.js
@@ -2,36 +2,6 @@ import "./assets/styles/style.css";
 import "./assets/styles/ImageStyling.scss";
 let display = document.getElementById("textToImage");
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.message === "postData") {
-    console.log(message.data);
-    let array = [];
-    array.push(message.data);
-    console.log(array);
-
-    let html = "";
-
-    array.map((item, index) => {
-      html += `
-        <div class="imageDisplay">
-        <div class="userDetail">
-           <img>
-          <span>${item.username}</span>
-          <span>${item.handle}</span>
-          <span>${item.timestamp}</span>
-</div>
-<p class="tweetText">${item.tweetText}</p>
-<div class="tweetExpressions">  <span><i class="fas fa-message" aria-hidden="true"></i>${item.reply}</span>
-          <span><i class="fas fa-retweet" aria-hidden="true"></i>${item.retweet}</span>
-          <span><i class="fas fa-heart" aria-hidden="true"></i>${item.likesCount}</span>
-          <span><i class="fas fa statistics" aria-hidden="true"></i>${item.statisitics}</span></div>
-        </div>
-      `;
-    });
-    display.innerHTML = html;
-  }
-});
-
 let loginBtn = document.getElementById("login-btn");
 let logoutBtn = document.getElementById("logout-btn");
 
@@ -68,4 +38,34 @@ logoutBtn.addEventListener("click", () => {
     message: "logout",
     token: localStorage.getItem("token"),
   });
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.message === "postData") {
+    console.log(message.data);
+    let array = [];
+    array.push(message.data);
+    console.log(array);
+
+    let html = "";
+
+    array.map((item, index) => {
+      html += `
+        <div class="imageDisplay">
+        <div class="userDetail">
+           <img>
+          <span>${item.username}</span>
+          <span>${item.handle}</span>
+          <span>${item.timestamp}</span>
+</div>
+<p class="tweetText">${item.tweetText}</p>
+<div class="tweetExpressions">  <span><i class="fas fa-message" aria-hidden="true"></i>${item.reply}</span>
+          <span><i class="fas fa-retweet" aria-hidden="true"></i>${item.retweet}</span>
+          <span><i class="fas fa-heart" aria-hidden="true"></i>${item.likesCount}</span>
+          <span><i class="fas fa statistics" aria-hidden="true"></i>${item.statisitics}</span></div>
+        </div>
+      `;
+    });
+    display.innerHTML = html;
+  }
 });
