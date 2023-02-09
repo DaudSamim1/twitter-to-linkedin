0ee81a969314fc828d19dcfe4772fea7d6ca3fe4
Aqsa-Irshad09 <--global>
2023-02-09 17:18:10 +0500

    stlying image generation

--- src/content.js
+++ src/content.js
@@ -7,7 +7,7 @@ let initialInterval = setInterval(() => {
       setTimeout(() => {
         if (document.querySelector("[data-testid='Dropdown']")) {
           let parentDiv = document.querySelector("[data-testid='Dropdown']");
-          
+
           let child = parentDiv.querySelector(".btn_unique");
           if (!child) {
             sendButton = document.querySelector("[data-testid='Dropdown']");
@@ -29,6 +29,14 @@ let initialInterval = setInterval(() => {
 
             // Add a click event listener to the custom button
             btnSend.addEventListener("click", () => {
+              let array = [];
+
+              var parentDiv = document.querySelector(
+                "div[data-testid='User-Names']"
+              ).innerText;
+              array.push(parentDiv);
+              let separatedStrings = array[0].split("\n");
+
               // Get the data of the Twitter post
               let articleT = item.closest(
                 "article[data-testid='tweet']"
@@ -38,26 +46,25 @@ let initialInterval = setInterval(() => {
               let imageUrl = avatar.src;
               let tweetText = articleT.querySelector(
                 "div[data-testid='tweetText']"
-              ).innerText;
-              let username = articleT.querySelector(
-                ".css-901oao.r-1awozwy.r-18jsvk2.r-6koalj.r-37j5jr.r-a023e6.r-b88u0q.r-rjixqe.r-bcqeeo.r-1udh08x.r-3s2u2q.r-qvutc0"
-              ).innerText;
-              let handle = articleT.querySelector(
-                ".css-901oao.css-1hf3ou5.r-14j79pv.r-18u37iz.r-37j5jr.r-1wvb978.r-a023e6.r-16dba41.r-rjixqe.r-bcqeeo.r-qvutc0"
-              ).innerText;
+              );
+              try {
+                tweetText = tweetText.innerText;
+              } catch (error) {
+                tweetText = null;
+              }
+              let username = separatedStrings[0];
+              let handle = separatedStrings[1];
               let reply = articleT.querySelector(
                 "div[data-testid='reply']"
               ).innerText;
-              let timestamp = articleT.querySelector(
-                ".css-1dbjc4n.r-18u37iz.r-1q142lx"
-              ).innerText;
+              let timestamp = separatedStrings[3];
               let retweet = articleT.querySelector(
                 "div[data-testid='retweet']"
               ).innerText;
               let likesCount = articleT.querySelector(
                 "div[data-testid='like']"
               ).innerText;
-           
+
               let tweetData = {
                 avatar: imageUrl,
                 username: username,
@@ -67,7 +74,6 @@ let initialInterval = setInterval(() => {
                 reply: reply,
                 retweet: retweet,
                 likesCount: likesCount,
-              
               };
 
               chrome.runtime.sendMessage({
--- src/popup.js
+++ src/popup.js
@@ -2,7 +2,7 @@ import "./assets/styles/customButtonstyle.scss";
 
 let loginBtn = document.getElementById("login-btn");
 let logoutBtn = document.getElementById("logout-btn");
-
+window.location.href = "dashboard.html";
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.message === "postData") {
     window.location.href = "dashboard.html";
@@ -42,8 +42,8 @@ logoutBtn.addEventListener("click", () => {
   });
 });
 
-let NextBtn=document.getElementById("nextBtn")
+let NextBtn = document.getElementById("nextBtn");
 
-NextBtn.addEventListener("click" ,() =>{
-  window.location.href=chrome.runtime.getURL("signuppassword.html");
-})
+NextBtn.addEventListener("click", () => {
+  window.location.href = chrome.runtime.getURL("signuppassword.html");
+});
